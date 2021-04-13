2dac6a09ded32570f59f5a57f421a0d77d8e97d1
yakupensarsayin <yakupensarsayin@hotmail.com>
2021-04-13 21:47:55 +0300

    Add project files.

---
+++ MyDictionary.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<K, V>
+    {
+        K[] keysArray;
+        V[] valuesArray;
+        public MyDictionary()
+        {
+            keysArray = new K[0];
+            valuesArray = new V[0];
+        }
+
+        public void Add(K key, V value)
+        {
+            K[] tempKeys = keysArray;
+            V[] tempValues = valuesArray;
+
+            keysArray = new K[keysArray.Length + 1];
+            valuesArray = new V[valuesArray.Length + 1];
+
+            for (int i = 0; i < tempKeys.Length; i++)
+            {
+                keysArray[i] = tempKeys[i];
+                valuesArray[i] = tempValues[i];
+            }
+
+            keysArray[keysArray.Length - 1] = key;
+            valuesArray[valuesArray.Length - 1] = value;
+        }
+
+        public int LengthOfKeys
+        {
+            get { return keysArray.Length; }
+        }
+        public int LengthOfValues
+        {
+            get { return valuesArray.Length; }
+        }
+        public K[] keys
+        {
+            get { return keysArray; }
+        }
+        public V[] values
+        {
+            get { return valuesArray; }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> students = new MyDictionary<int, string>();
+            students.Add(120, "Merry K. Sullivan");
+            students.Add(247, "Leola J. Ortiz");
+            students.Add(359, "Sam G. Hall");
+            students.Add(481, "George E.Olsen");
+            students.Add(667, "Andrew L. Powell");
+
+            Console.WriteLine("We have {0} students in our system.", students.LengthOfKeys);
+
+            for (int i = 0; i < students.LengthOfKeys; i++)
+            {
+                Console.WriteLine("Student's ID: {0} | Student's Name: {1}", students.keys[i], students.values[i]);
+            }
+        }
+    }
+}
